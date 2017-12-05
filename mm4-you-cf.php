d354533ecc9704788aed66433ae0b52d551b7f2e
cstielper <chris@mm4solutions.com>
2017-12-05 14:05:31 -0500

    Disable RECAPTCHA fields if user is not an admin

--- mm4-you-cf.php
+++ mm4-you-cf.php
@@ -100,17 +100,19 @@ function thank_you_page_callback() {
 }
 
 function recaptcha_public_key_callback() {
+	if(!is_admin()): $disabled = 'disabled'; endif;
 	$settings = (array) get_option( 'mm4-you-cf-settings' );
 	$field = "recaptcha-public-key";
 	$value = esc_attr( $settings[$field] );
-	echo "<input type='text' name='mm4-you-cf-settings[$field]' value='$value' />";
+	echo "<input type='text' name='mm4-you-cf-settings[$field]' value='$value' $disabled />";
 }
 
 function recaptcha_private_key_callback() {
+	if(!is_admin()): $disabled = 'disabled'; endif;
 	$settings = (array) get_option( 'mm4-you-cf-settings' );
 	$field = "recaptcha-private-key";
 	$value = esc_attr( $settings[$field] );
-	echo "<input type='text' name='mm4-you-cf-settings[$field]' value='$value' />";
+	echo "<input type='text' name='mm4-you-cf-settings[$field]' value='$value' $disabled />";
 }
 
 // Grab the settings from our options page
